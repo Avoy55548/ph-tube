724f3b1e6b29bafd2aec26e08e1c5f4774e87855
Tasin Islam Avoy <avoytasin@gmail.com>
2025-03-13 21:50:28 +0600

    video API done

--- script/index.js
+++ script/index.js
@@ -10,6 +10,7 @@ function loadCategories(){
 }
 
 
+//display function
 function displayCategories(categories){
  //get the container
     const categoryContainer= document.getElementById("category-container");
@@ -31,4 +32,47 @@ function displayCategories(categories){
 
 
 }
+
+
+
+
+
+function loadVideos(){
+    fetch("https://openapi.programming-hero.com/api/phero-tube/videos")
+    .then((res) => res.json())
+    .then((data) => displayVideos(data.videos));
+}
+
+const displayVideos=(videos) =>{
+    const videoContainer= document.getElementById("video-container");
+
+    for(video of videos){
+        
+        // create element
+        const videoCard = document.createElement("div");
+        videoCard.innerHTML=`
+        <div class="card bg-base-100  shadow-sm">
+  <figure>
+    <img
+      src="${video.thumbnail}"
+      alt="Shoes" />
+  </figure>
+  <div class="card-body">
+    <h2 class="card-title">${video
+        .title
+    }</h2>
+    <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
+    <div class="card-actions justify-end">
+      <button class="btn btn-primary">Buy Now</button>
+    </div>
+  </div>
+</div>
+        `
+        // append 
+        videoContainer.append(videoCard);
+    }
+}
+
+
 loadCategories();
+loadVideos();
